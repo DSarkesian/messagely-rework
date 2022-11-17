3938893c701e59c6ec3889ef031111f8752b8a85
Daniel Sarkesian <danielsarkesian@gmail.com>
2022-11-17 18:45:33 -0500

    finished users models

--- models/user.js
+++ models/user.js
@@ -16,8 +16,7 @@ class User {
   static async register({ username, password, first_name, last_name, phone }) {
     const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
     const result = await db.query(`
-    INSERT INTO users (
-      username,
+    INSERT INTO users ( username,
       password,
       first_name,
       last_name,
@@ -26,10 +25,10 @@ class User {
       last_login_at)
       VALUES
         ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
-      RETURNING (username, password, first_name, last_name, phone) `,
+      RETURNING username, password, first_name, last_name, phone `,
       [username, hashedPassword, first_name, last_name, phone]);
 
-      return result.rows
+      return result.rows[0]
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
@@ -111,6 +110,34 @@ class User {
    */
 
   static async messagesFrom(username) {
+    const result = await db.query(`
+    SELECT u.username,
+           u.first_name,
+           u.last_name,
+           u.phone,
+           m.id,
+           m.body,
+           m.sent_at,
+           m.read_at
+    FROM messages m
+    JOIN users u ON  m.to_username = u.username
+    WHERE from_username = $1`,
+    [username]);
+
+    return result.rows.map(m=>({
+      id:m.id,
+      to_user:{
+        username: m.username,
+        first_name:m.first_name,
+        last_name:m.last_name,
+        phone:m.phone
+      },
+      body:m.body,
+      sent_at:m.sent_at,
+      read_at:m.read_at
+    }));
+
+
   }
 
   /** Return messages to this user.
@@ -122,6 +149,30 @@ class User {
    */
 
   static async messagesTo(username) {
+    const results = await db.query(`
+      SELECT u.username,
+            u.first_name,
+              u.last_name,
+              u.phone,
+              m.id,
+              m.body,
+              m.sent_at,
+              m.read_at
+      FROM messages m
+      JOIN users u ON m.from_username = u.username
+      WHERE m.to_username = $1`,
+      [username]);
+      return results.rows.map(m=>({
+        id:m.id,
+        from_user:{
+          username:m.username,
+          first_name:m.first_name,
+          last_name:m.last_name,
+          phone:m.phone},
+        body:m.body,
+        sent_at:m.sent_at,
+        read_at:m.read_at
+      }));
   }
 }
 
