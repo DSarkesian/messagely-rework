7124fd4b8ad57835f69d27eb8ef0e7027781273c
Daniel Sarkesian <danielsarkesian@gmail.com>
2022-11-18 20:15:25 -0500

    finished auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const User = require("../models/user");
 const Router = require("express").Router;
 const router = new Router();
 
-
+/** POST /login: {username, password} => {token} */
 router.post('/login', async function(req,res,next){
   const {username, password} = req.body;
   if(await User.authenticate(username,password)){
@@ -19,12 +19,20 @@ router.post('/login', async function(req,res,next){
     throw new UnauthorizedError("Invalid username");
   }
 })
-/** POST /login: {username, password} => {token} */
+
 
 
 /** POST /register: registers, logs in, and returns token.
- *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+
+router.post('/register', async function(req,res,next){
+  const user = User.register(req.body);
+  const token = jwt.sign({ username }, SECRET_KEY);
+  User.updateLoginTimestamp(username);
+  return res.json({ token });
+})
+
+
 
 module.exports = router;
