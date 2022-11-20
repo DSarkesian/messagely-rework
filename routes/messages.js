9df085ed8df778b64fb801b3ddb2cf3757ff96d4
Daniel Sarkesian <danielsarkesian@gmail.com>
2022-11-19 23:40:33 -0500

    messages routes

--- routes/messages.js
+++ routes/messages.js
@@ -3,35 +3,36 @@
 const Router = require("express").Router;
 const router = new Router();
 
-/** GET /:id - get detail of message.
+router.get("/", ensureLoggedIn, async function (req, res, next) {
+  let users = await User.all();
+  return res.json({ users });
+});
+
+/** GET /:username - get detail of users.
  *
- * => {message: {id,
- *               body,
- *               sent_at,
- *               read_at,
- *               from_user: {username, first_name, last_name, phone},
- *               to_user: {username, first_name, last_name, phone}}
- *
- * Makes sure that the currently-logged-in users is either the to or from user.
+ * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
 
+router.get("/:username", ensureCorrectUser, async function (req, res, next) {
+  let user = await User.get(req.params.username);
+  return res.json({ user });
+});
 
-/** POST / - post message.
+/** GET /:username/to - get messages to user
  *
- * {to_username, body} =>
- *   {message: {id, from_username, to_username, body, sent_at}}
+ * => {messages: [{id,
+ *                 body,
+ *                 sent_at,
+ *                 read_at,
+ *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
 
-
-/** POST/:id/read - mark message as read:
- *
- *  => {message: {id, read_at}}
- *
- * Makes sure that the only the intended recipient can mark as read.
- *
- **/
+router.get("/:username/to", ensureCorrectUser, async function (req, res, next) {
+  let messages = await User.messagesTo(req.params.username);
+  return res.json({ messages });
+});
 
 
 module.exports = router;
